1941ee415cbb072a505f438d62def375b53a9cc6
Yerzhan <yerzhan.aday@gmail.com>
2024-02-20 08:51:39 +0600

    add stylelint, i18n plugin for eslint, fix all styles

--- src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,5 +1,4 @@
 import {classNames} from "shared/lib/classNames/classNames";
-import cls from './LangSwitcher.module.scss';
 import {useTranslation} from "react-i18next";
 import {Button, ThemeButton} from "shared/ui/Button/Button";
 
@@ -11,12 +10,13 @@ export const LangSwitcher = ({className}: LangSwitcherProps) => {
     const { t, i18n} = useTranslation();
 
     const toggle = () => {
+        // eslint-disable-next-line i18next/no-literal-string
         i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
     };
 
     return (
         <Button
-            className={classNames(cls.LangSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             theme={ThemeButton.CLEAR}
             onClick={toggle}
         >
