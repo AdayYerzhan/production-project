2c1a645a5c50498cba7a025a1933229dadc0ea24
Yerzhan <yerzhan.aday@gmail.com>
2024-02-22 08:38:41 +0600

    add page loader, add not found page, add error boundray

--- src/pages/MainPage/ui/MaingPage.tsx
+++ src/pages/MainPage/ui/MaingPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {useTranslation} from "react-i18next";
+import {BugButton} from "app/providers/ErrorBoundary";
 
 const MaingPage = () => {
     const {t, i18n} = useTranslation();
 
     return (
         <div>
+            <BugButton />
             {t("Главаная страница")}
         </div>
     );
